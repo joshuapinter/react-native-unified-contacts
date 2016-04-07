97c3b24d10872d3ae74d707048cb1e82f6eb3ba7
Joshua Pinter <joshuapinter@gmail.com>
2016-04-07 11:00:44 -0600

    Remove comments.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 var ReactNative = require('react-native')
 
-console.log( 'index.js of react-native-unified-contacts' );
-console.dir( ReactNative.NativeModules );
-
 module.exports = ReactNative.NativeModules.RNUnifiedContacts
