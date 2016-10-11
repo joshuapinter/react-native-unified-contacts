7ffb738eb4e6a94e2b1cba3fffeb5bb2fc66c340
Joshua Pinter <joshuapinter@gmail.com>
2016-10-11 16:29:54 -0600

    Add ExampleApp to show you what your iOS project should look like.

---
+++ ExampleApp/index.ios.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var Contacts  = require('react-native-unified-contacts');
+
+Contacts.getContacts( (error, contacts) =>  {
+  if (error) {
+    console.error(error);
+  }
+  else {
+    console.log(contacts);
+  }
+});
+
+
+export default class ExampleApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ExampleApp', () => ExampleApp);
