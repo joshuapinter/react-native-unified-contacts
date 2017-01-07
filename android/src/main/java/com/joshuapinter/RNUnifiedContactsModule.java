72fba993bc4f503d33f479e1ca0cfc4d711ef022
Joshua Pinter <joshuapinter@gmail.com>
2017-01-07 12:58:24 -0700

    Basics of reading sent SMS messages.
    Just to test if it’s possible, which it is. Will have to do a tonne of work to get this working without a hitch but this could be super powerful.

--- android/src/main/java/com/joshuapinter/RNUnifiedContactsModule.java
+++ android/src/main/java/com/joshuapinter/RNUnifiedContactsModule.java
@@ -1,8 +1,15 @@
 package com.joshuapinter;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -70,4 +77,36 @@ public class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
             currentActivity.startActivityForResult(intent, 1);
         }
     }
+
+    @ReactMethod
+    public void getMessages() {
+        Activity currentActivity = getCurrentActivity();
+
+        if (ContextCompat.checkSelfPermission(currentActivity.getBaseContext(), "android.permission.READ_SMS") != PackageManager.PERMISSION_GRANTED) {
+            Log.w("Test3", "request permission");
+            final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+            ActivityCompat.requestPermissions(currentActivity, new String[]{"android.permission.READ_SMS"}, REQUEST_CODE_ASK_PERMISSIONS);
+        }
+
+//        ActivityCompat.requestPermissions(currentActivity,
+//                new String[]{Manifest.permission.READ_SMS},
+//                1);
+
+        Cursor cursor = currentActivity.getContentResolver().query(Uri.parse("content://sms/sent"), null, null, null, null);
+
+        if (cursor.moveToFirst()) { // must check the result to prevent exception
+            do {
+                String msgData = "";
+                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                }
+
+                Log.w("Test1", msgData);
+            } while (cursor.moveToNext());
+        } else {
+            Log.w("Test2", "Messages empty.");
+            // empty box, no SMS
+        }
+    }
+
 }
