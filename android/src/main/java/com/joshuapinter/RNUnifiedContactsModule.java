7b74a95dd95bc1899eb3edb63a48a1af38ba5460
Joshua Pinter <joshuapinter@gmail.com>
2017-01-09 09:30:18 -0700

    Selecting contact and returning display name.
    Just playing around with it right now. Not production code.

--- android/src/main/java/com/joshuapinter/RNUnifiedContactsModule.java
+++ android/src/main/java/com/joshuapinter/RNUnifiedContactsModule.java
@@ -1,7 +1,7 @@
 package com.joshuapinter;
 
-import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -12,6 +12,10 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -21,9 +25,88 @@ import java.util.Map;
 
 public class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
 
+    private Promise mSelectContactPromise;
+    private Callback mSuccessCallback;
+    private Callback mErrorCallback;
+
     public RNUnifiedContactsModule(ReactApplicationContext reactContext) {
         super(reactContext);
+
+        reactContext.addActivityEventListener(mActivityEventListener);
     }
+
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+            // Get the URI and query the content provider for the phone number
+            Uri contactUri = data.getData();
+
+            ContentResolver contentResolver = activity.getContentResolver();
+
+            Cursor contactCursor = contentResolver.query(contactUri, null, null, null, null);
+
+            contactCursor.moveToFirst();
+
+            int columnId = contactCursor.getColumnIndex(ContactsContract.Contacts._ID);
+            String contactId = contactCursor.getString(columnId);
+
+            int columnDisplayName = contactCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+            String contactDisplayName = contactCursor.getString(columnDisplayName);
+
+            Log.w("Test1", contactDisplayName);
+            mSuccessCallback.invoke(contactDisplayName);
+
+//            String[] projection = new String[]{ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME};
+//            Cursor cursor = activity.getContentResolver().query(contactUri, projection,
+//                    null, null, null);
+
+
+
+//            // If the cursor returned is valid, get the phone number
+//            if (cursor != null && cursor.moveToFirst()) {
+//                int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+//                String name = cursor.getString(nameIndex);
+//
+//                Log.w("Test1", "name:");
+//                Log.w("Test1", name);
+//                // Do something with the phone number
+//                mSuccessCallback.invoke(name);
+//            }
+//
+//            Log.w("Test1", "onActivityResult after");
+//            mSelectContactPromise.resolve("Resolve1");
+        }
+    };
+
+
+//            // column index of the phone number
+//            Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+//                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+//                    new String[]{id}, null);
+//            while (pCur.moveToNext()) {
+//                String phone = pCur.getString(
+//                        pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                txtTelefono.setText(phone);         //print data
+//            }
+//            pCur.close();
+//            // column index of the email
+//            Cursor emailCur = cr.query(
+//                    ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+//                    null,
+//                    ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+//                    new String[]{id}, null);
+//            while (emailCur.moveToNext()) {
+//                // This would allow you get several email addresses
+//                // if the email addresses were stored in an array
+//                String email = emailCur.getString(
+//                        emailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS));
+//                txtMailContacto.setText(email);         //print data
+//            }
+//            emailCur.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 
     @Override
@@ -68,7 +151,10 @@ public class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void selectContact() {
+    public void selectContact(Callback errorCallback, Callback successCallback) {
+        mErrorCallback = errorCallback;
+        mSuccessCallback = successCallback;
+
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
         Activity currentActivity = getCurrentActivity();
