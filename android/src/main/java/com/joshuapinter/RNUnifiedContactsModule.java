8d7912b0aaa3ececbee197e05c592691ff5c66ab
Joshua Pinter <joshuapinter@gmail.com>
2017-01-13 22:54:07 -0700

    Return contact details including phone numbers.
    Still not complete.

--- android/src/main/java/com/joshuapinter/RNUnifiedContactsModule.java
+++ android/src/main/java/com/joshuapinter/RNUnifiedContactsModule.java
@@ -1,6 +1,5 @@
 package com.joshuapinter;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -8,18 +7,21 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,8 +31,7 @@ class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
     private Promise mSelectContactPromise;
     private Callback mSuccessCallback;
     private Callback mErrorCallback;
-
-
+    private ContentResolver contentResolver;
 
     public RNUnifiedContactsModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -41,10 +42,11 @@ class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
         @Override
         public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-            // Get the URI and query the content provider for the phone number
             Uri contactUri = data.getData();
 
-            ContentResolver contentResolver = activity.getContentResolver();
+            contentResolver = activity.getContentResolver();
+
+//            String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME};
 
             Cursor contactCursor = contentResolver.query(contactUri, null, null, null, null);
 
@@ -53,11 +55,18 @@ class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
             int columnId = contactCursor.getColumnIndex(ContactsContract.Contacts._ID);
             String contactId = contactCursor.getString(columnId);
 
-            int columnDisplayName = contactCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
-            String contactDisplayName = contactCursor.getString(columnDisplayName);
+            WritableMap contactMap = getContactDetailsFromContactId(contactId);
 
-            Log.w("Test11", contactDisplayName);
-            mSuccessCallback.invoke(contactDisplayName);
+//            int columnDisplayName = contactCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+//            String contactDisplayName = contactCursor.getString(columnDisplayName);
+
+//            String family_name =       contactCursor.getString(contactCursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME));
+
+
+//            int columnStructredName = contactCursor.getColumnCount(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+
+            Log.w("Test12", contactMap.toString());
+            mSuccessCallback.invoke(contactMap);
 
 //            String[] projection = new String[]{ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME};
 //            Cursor cursor = activity.getContentResolver().query(contactUri, projection,
@@ -80,6 +89,79 @@ class RNUnifiedContactsModule extends ReactContextBaseJavaModule {
 //            mSelectContactPromise.resolve("Resolve1");
         }
     };
+
+    private WritableMap getContactDetailsFromContactId(String contactId) {
+        WritableMap contactMap = Arguments.createMap();
+
+        String whereName = ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID + " = ?";
+        String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, contactId };
+        Cursor nameCur = contentResolver.query(ContactsContract.Data.CONTENT_URI, null, whereName, whereNameParams, ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+        while (nameCur.moveToNext()) {
+            String given = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME));
+            String family = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME));
+//            String display = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+            String middle = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.MIDDLE_NAME));
+
+            contactMap.putString( "identifier", contactId );
+            contactMap.putString( "familyName", family );
+            contactMap.putString( "middleName", middle );
+            contactMap.putString( "givenName", given );
+
+            WritableArray phoneNumbers = getPhoneNumbersFromContact(contactId);
+            contactMap.putArray( "phoneNumbers", phoneNumbers );
+
+            Log.w("Test13", contactMap.toString());
+        }
+        nameCur.close();
+
+        return contactMap;
+    }
+
+    @NonNull
+    private WritableArray getPhoneNumbersFromContact(String contactId) {
+        WritableArray phoneNumbers = Arguments.createArray();
+
+        Cursor phoneNumberCursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId,
+                null,
+                null);
+
+        while (phoneNumberCursor.moveToNext()) {
+            WritableMap phoneNumber = Arguments.createMap();
+
+            String number = phoneNumberCursor.getString(phoneNumberCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            String normalizedNumber = phoneNumberCursor.getString(phoneNumberCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NORMALIZED_NUMBER));
+
+            String label = phoneNumberCursor.getString(phoneNumberCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.LABEL));
+
+            phoneNumber.putString("stringValue", number);
+            phoneNumber.putString("digits", normalizedNumber);
+            phoneNumber.putString("label", label);
+
+            int typeInt = phoneNumberCursor.getInt(phoneNumberCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+
+            String type = String.valueOf(ContactsContract.CommonDataKinds.Phone.getTypeLabel(getCurrentActivity().getResources(), typeInt, ""));
+            phoneNumber.putString("type", type);
+
+//            switch (type) {
+//                case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+//                    // do something with the Home number here...
+//                    break;
+//                case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+//                    // do something with the Mobile number here...
+//                    break;
+//                case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+//                    // do something with the Work number here...
+//                    break;
+//            }
+
+            phoneNumbers.pushMap(phoneNumber);
+        }
+        phoneNumberCursor.close();
+        return phoneNumbers;
+    }
 
 
 //            // column index of the phone number
